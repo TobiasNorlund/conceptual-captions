056845a6efca10bc704b8394981b784d749165f0
Tobias Norlund <tobias@norlund.se>
2020-11-25 16:28:03 +0100

    Script to download using parallel command

---
+++ README.md
@@ -0,0 +1,6 @@
+# Download script for Conceptual Captions dataset
+
+Usage: 
+```bash
+./download.sh path/to/cc.tsv output/dir
+```
---
+++ download.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if ! test -f "$1"; then
+    echo "ERROR: Input file '$1' does not exist (first argument)"
+    exit 1
+fi
+
+if test -z "$2" ; then 
+    echo "ERROR: No output dir set (second argument)"
+    exit 1
+fi
+
+# Make sure output dir exists
+OUTPUT_DIR="$2"
+[[ "${OUTPUT_DIR}" != */ ]] && OUTPUT_DIR="${OUTPUT_DIR}/"
+mkdir -p $2
+
+# Download
+NUM_PROCESSES=$(expr $(grep -c ^processor /proc/cpuinfo) \* 4)
+cat $1 | cut -d'	' -f2 | parallel -j $NUM_PROCESSES --joblog joblog --bar --resume --resume-failed wget -T 10 -O $OUTPUT_DIR{#} -o /dev/null -A jpeg,jpg,bmp,gif,png {}
+
+# Remove non-image files
+FAILED_DOWNLOADS_COUNT=$(find $OUTPUT_DIR -type f | xargs file | grep -v image | cut -d: -f1 | wc -l)
+find $OUTPUT_DIR -type f | xargs file | grep -v image | cut -d: -f1 | xargs rm
+
+echo "Download finished with $FAILED_DOWNLOADS_COUNT failed URLs"
